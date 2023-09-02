eb0573d9c31d23f228fff80f1e97720b8fa53120
Yulduz Buranbaeva <130300539+Yulduzb@users.noreply.github.com>
2023-09-02 14:49:25 +0300

    add readme

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
-# Astronomy-Picture
-# Astronomy-Picture
+
+
+<h1>Astronomy Picture of the Day</h1>
+
+<p>This JavaScript project, "Astronomy Picture of the Day," allows you to explore the fascinating world of astronomy and space by providing you with a daily image along with interesting information about it.</p>
+
+<h2>Technologies Used</h2>
+<h3>The website was developed using the following technologies:</h3>
+
+HTML
+CSS
+Javascript
+
+![](space.gif)
+
+
--- script.js
+++ script.js
@@ -65,7 +65,7 @@ const showDetails=(detail)=>{
   
  <div class="image-data">
  
-     <img src="${detail.hdurl}" alt="">
+     <img src="${detail.hdurl}" alt="" loading="lazy"">
  </div>
  <div class="img-details">
      <h2 class="data-title">${detail.title}</h2>
@@ -94,7 +94,7 @@ const showDetails=(detail)=>{
 
 
   const getPicture = async () => {      
-    const url = `https://api.nasa.gov/planetary/apod?api_key=187vZS1Rm7Y6cEluSk2VucgxgJrs4tCeV4iI4mpI&start_date=2023-08-01&end_date=2023-08-30`;
+    const url = `https://api.nasa.gov/planetary/apod?api_key=Yourkey&start_date=2023-08-01&end_date=2023-08-30`;
     
     try {
          const response = await fetch(url);
