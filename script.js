d463106fea7c66b0cac0254488ac2d12b03c61ba
Yulduz Buranbaeva <130300539+Yulduzb@users.noreply.github.com>
2023-09-02 13:41:07 +0300

    add priwiew card popup

--- script.js
+++ script.js
@@ -1,30 +1,24 @@
-
 const btn=document.querySelector(".btnSearch");
 const inputDate=document.querySelector("input");
 const images=document.querySelector(".images");
-
-
+const detailsContainer=document.querySelector(".details-container ");
 
 
 let selectedImageData=null;
 
 const dataArray=[];
 
-
-
-
+document.addEventListener("DOMContentLoaded", function () {
 btn.addEventListener("click",() => {
 
 const filteredData=dataArray.filter((dateItem) => dateItem.date === inputDate.value)
-console.log(filteredData);
-console.log(typeof filteredData);
-  displayFilteredData(filteredData);
-  
+console.log(filteredData)
+displayFilteredData(filteredData);
 
  
 })
 
-
+});
 
 const displayFilteredData =(filteredData)=>{
   images.innerHTML="";
@@ -34,8 +28,11 @@ filteredData.forEach((dateItem)=>{
   imagesDiv.classList.add("image-container");
 
   const imagesDivInnerHtml=`
+  
   <div class="image">
+
   <img src="${dateItem.hdurl}" alt="${dateItem.title}" >
+  
   </div>
   <div class="img-details">
       <h2 class="title">${dateItem.title}</h2>
@@ -47,12 +44,53 @@ filteredData.forEach((dateItem)=>{
  imagesDiv.innerHTML=imagesDivInnerHtml;
  images.appendChild(imagesDiv);
  
+
+imagesDiv.addEventListener("click",() => {
+selectedImageData = dateItem;
+showDetails(selectedImageData);
+
+ })
   
  })
   
 }
 
+const showDetails=(detail)=>{
+ const containerData=document.createElement("div");
+ containerData.classList.add("container-data");
 
+ const dataInnerHtml=`
+
+ <button class="closeBtn"><i class="fa-solid fa-xmark"></i></button>
+  
+ <div class="image-data">
+ 
+     <img src="${detail.hdurl}" alt="">
+ </div>
+ <div class="img-details">
+     <h2 class="data-title">${detail.title}</h2>
+     <span class="data-date">${detail.date}</span>
+     <span class="data-copyright">${detail.copyright}</span>
+     <p class="data-explanation" >${detail.explanation} </p>
+ </div>
+
+ 
+ `
+
+ containerData.innerHTML=dataInnerHtml;
+ detailsContainer.appendChild(containerData);
+
+
+ const closeBtn=detailsContainer.querySelector(".closeBtn");
+ closeBtn.addEventListener("click", function(){
+  detailsContainer.removeChild(containerData);
+ })
+  
+  
+
+
+
+}
 
 
   const getPicture = async () => {      
@@ -64,6 +102,7 @@ filteredData.forEach((dateItem)=>{
           dataArray.push(...data);
        
            displayFilteredData(dataArray)
+           
 
         
          
